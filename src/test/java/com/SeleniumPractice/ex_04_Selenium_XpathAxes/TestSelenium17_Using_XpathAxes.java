f5da7d1dd3ffe755ff638a4cc15b56861571b24a
Prathyusha <tprathyushareddy31@gmail.com>
2025-04-23 23:32:28 +0530

    Selenium xpathaxes and cssselectors practice

--- src/test/java/com/SeleniumPractice/ex_04_Selenium_XpathAxes/TestSelenium17_Using_XpathAxes.java
+++ src/test/java/com/SeleniumPractice/ex_04_Selenium_XpathAxes/TestSelenium17_Using_XpathAxes.java
@@ -4,21 +4,66 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import java.util.List;
 
 public class TestSelenium17_Using_XpathAxes {
 
-    @Test
-    public void test_Selenium_XpathAxes() {
-        WebDriver driver = new EdgeDriver();
+
+    WebDriver driver;
+
+    @BeforeTest
+    public void launchBrowser() {
+        driver = new EdgeDriver();
         driver.manage().window().maximize();
         driver.get("https://awesomeqa.com/xpath/");
+    }
+
+    @Test
+    public void test_Selenium_XpathAxes() {
 
         List<WebElement> ancestors = driver.findElements(By.xpath("//div[@class='Mammal']/ancestor::div"));
         for (WebElement a : ancestors) {
             System.out.println(a.getText());
+        }
+
+        System.out.println("----------------------------------------------");
+
+        WebElement following_sibling = driver.findElement(By.xpath("//div[@class='Mammal']/following-sibling::div"));
+        System.out.println(following_sibling.getText());
+
+        System.out.println("----------------------------------------------");
+
+        WebElement preceding_sibling = driver.findElement(By.xpath("//div[@class='Mammal']/preceding-sibling::div"));
+        System.out.println(preceding_sibling.getText());
+
+        System.out.println("----------------------------------------------");
+
+        List<WebElement> descendants = driver.findElements(By.xpath("//div[@class='Mammal']/descendant::div"));
+        for (WebElement d : descendants) {
+            System.out.println(d.getText());
+        }
+
+        System.out.println("----------------------------------------------");
+
+        List<WebElement> children = driver.findElements(By.xpath("//div[@class='Mammal']/child::div"));
+            for (WebElement c : children) {
+                System.out.println(c.getText());
+            }
+
+
+    }
+
+
+    @AfterTest
+            public void closeBrowser() {
+        try {
+            driver.quit();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
         }
     }
 }
--- src/test/java/com/SeleniumPractice/ex_05_Selenium_CSSselectors/TestSelenium18_CSSselectors.java
+++ src/test/java/com/SeleniumPractice/ex_05_Selenium_CSSselectors/TestSelenium18_CSSselectors.java
@@ -1,4 +1,63 @@
 package com.SeleniumPractice.ex_05_Selenium_CSSselectors;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
 public class TestSelenium18_CSSselectors {
+
+    WebDriver driver;
+
+    @BeforeTest
+    public void launchBrowser() {
+        driver = new EdgeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://app.vwo.com");
+    }
+
+    @Test
+    public void testMethod_withCSSselectors(){
+
+        // # --> id | Syntax: tagName#id
+        // .--> className | Syntax: tagName.className
+        WebElement username_field = driver.findElement(By.cssSelector("input#login-username"));
+        username_field.sendKeys("admin@123");
+
+        // Xpath to CSS -->  just remove // and @ from xpath then it becomes CSS selector
+
+       // Xpath: //input[@id="login-password"]   | CSS : input[id="login-password"] or just [id="login-password"]
+        WebElement password = driver.findElement(By.cssSelector("input[id='login-password']"));
+        password.sendKeys("admin");
+
+        //WebElement signin = driver.findElement(By.cssSelector("button[id='js-login-btn']"));
+        //to check for partial i.e similar to "contains" in xpath ---> instead of contain here we just use *
+        //similarly for starts-with --> ^
+        // and ends-with -->$
+
+      WebElement signin = driver.findElement(By.cssSelector("button[id*='js-login']"));
+          signin.click();
+
+        try {
+            Thread.sleep(4000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        WebElement errormessage = driver.findElement(By.cssSelector("div[class$='box-description']"));
+        Assert.assertEquals(errormessage.getText(),"Your email, password, IP address or location did not match");
+
+    }
+    @AfterTest
+    public void closeBrowser() {
+        try {
+            driver.quit();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
