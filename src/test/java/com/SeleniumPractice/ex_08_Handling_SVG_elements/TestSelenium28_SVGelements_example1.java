fd9de72436891b493f28bddf6fbd6d6eaefa9131
Prathyusha <tprathyushareddy31@gmail.com>
2025-04-27 19:33:20 +0530

    svg elements part2 practice

--- src/test/java/com/SeleniumPractice/ex_08_Handling_SVG_elements/TestSelenium28_SVGelements_example1.java
+++ src/test/java/com/SeleniumPractice/ex_08_Handling_SVG_elements/TestSelenium28_SVGelements_example1.java
@@ -1,11 +1,15 @@
 package com.SeleniumPractice.ex_08_Handling_SVG_elements;
 
+import com.SeleniumPractice.ex_09_waitHelpers.waitHelpers;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
+
+import java.util.List;
 
 public class TestSelenium28_SVGelements_example1 {
 
@@ -23,6 +27,22 @@ public class TestSelenium28_SVGelements_example1 {
         WebElement search = driver.findElement(By.xpath("//input[@type='text']"));
         search.sendKeys("macmini");
 
+        List<WebElement> svg = driver.findElements(By.xpath("//*[local-name()='svg']"));
+        svg.get(0).click();
+
+        waitHelpers.waitJVM(5000);
+
+        List<WebElement> titles = driver.findElements(By.xpath(" //div[contains(@data-id,'CPU')]/div/a[2]"));
+        // "//a[@class='wjcEIp']" --> xpath to get all the result titles
+        for(WebElement title : titles){
+            System.out.println(title.getText());
+        }
+    }
+
+    @AfterTest
+    public void closeBrowser(){
+        waitHelpers.waitJVM(3000);
+        driver.quit();
     }
 
 }
--- src/test/java/com/SeleniumPractice/ex_08_Handling_SVG_elements/TestSelenium29_SVGelements_example2.java
+++ src/test/java/com/SeleniumPractice/ex_08_Handling_SVG_elements/TestSelenium29_SVGelements_example2.java
@@ -1,4 +1,34 @@
 package com.SeleniumPractice.ex_08_Handling_SVG_elements;
 
+import com.SeleniumPractice.ex_09_waitHelpers.waitHelpers;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
 public class TestSelenium29_SVGelements_example2 {
+
+    @Test
+    public void test_svgMAP()
+    {
+        WebDriver driver = new EdgeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://www.amcharts.com/svg-maps/?map=india");
+
+        waitHelpers.waitJVM(3000);
+
+        List<WebElement> states = driver.findElements(By.xpath("//*[name()=\"svg\"]/*[name()=\"g\"][7]/*[name()=\"g\"]/*[name()=\"g\"]/*[name()=\"path\"]"));
+        for(WebElement state : states){
+            System.out.println(state.getDomAttribute("aria-label"));
+            if(state.getDomAttribute("aria-label").contains("Tripura")){
+                state.click();
+            }
+        }
+
+        waitHelpers.waitJVM(5000);
+        driver.quit();
+    }
 }
